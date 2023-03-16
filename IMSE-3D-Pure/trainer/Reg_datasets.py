615c1514f053fdab47fd809d81e37fd3ea5e820f
Kid Liet <52225942+Kid-Liet@users.noreply.github.com>
2023-03-16 11:20:26 +0800

    Update Reg_datasets.py

--- IMSE-3D-Pure/trainer/Reg_datasets.py
+++ IMSE-3D-Pure/trainer/Reg_datasets.py
@@ -33,7 +33,6 @@ class ImageDataset(Dataset):
         ############ 
         # make different non-affine to A and B
         item_A = self._NonAffine(imgs = [item_A],padding_modes=['border'],opt = self.opt)
-        #keep same deformation for A and B
         item_B = self._NonAffine(imgs = [item_B],padding_modes=['border'],opt = self.opt)
         return item_A, item_B
     def __len__(self):
@@ -67,7 +66,6 @@ class TestDataset(Dataset):
         item_Mask_B = self.transforms(np.load(patients+"/label.npy").astype(np.float32)) # MASK
         item_Mask_A[item_Mask_A > 0] = 1
         item_Mask_B[item_Mask_B > 0] = 1
-
         random_numbers = torch.rand(9).numpy() * 2 - 1
         item_A ,item_Mask_A = self._Affine(random_numbers=random_numbers,imgs = [item_A, item_Mask_A], 
                                             padding_modes=['border', 'zeros'],opt=self.opt)
